70b1b8c2383f9258a819a4ca9a968623187f22f6
Maximilian R <public@complexvector.space>
2023-11-13 13:21:15 +0100

    Add typst icon to presence

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,19 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	{ 'kaarmu/typst.vim', ft = "typst", lazy = false },
-	{ 'axelf4/vim-strip-trailing-whitespace' },
-	{ 'andweeb/presence.nvim' },
+  { 'kaarmu/typst.vim',
+    ft = "typst",
+    lazy = false
+  },
+  { 'axelf4/vim-strip-trailing-whitespace' },
+  {
+    'andweeb/presence.nvim',
+    config = function()
+      require("presence").setup({
+        file_assets = {
+          typ = { "Typst", "https://styles.redditmedia.com/t5_83esp8/styles/communityIcon_eytolmfg1gra1.png" }
+        }
+      })
+    end
+  },
 }
