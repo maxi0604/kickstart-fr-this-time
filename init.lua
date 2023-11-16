4b10d8430891b67fb56b89ff32121259813bbc1c
Maximilian R. <maxi.rostock@outlook.de>
2023-11-16 01:49:25 +0100

    Add custom options

--- init.lua
+++ init.lua
@@ -273,6 +273,9 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Import my options
+require("custom.opts")
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
