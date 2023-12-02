a6ce5099b843537595982265876348fa5d0f09f4
Maximilian R. <maxi.rostock@outlook.de>
2023-12-02 15:43:52 +0100

    Update

--- lua/custom/opts.lua
+++ lua/custom/opts.lua
@@ -1,1 +1,5 @@
 vim.o.showmode = false
+vim.o.expandtab = true
+vim.o.smarttab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
--- lua/custom/plugins/presence.lua
+++ lua/custom/plugins/presence.lua
@@ -12,7 +12,7 @@ return {
 
     require("presence").setup({
       file_assets = {
-        typ = { "Typst", "https://styles.redditmedia.com/t5_83esp8/styles/communityIcon_eytolmfg1gra1.png" }
+        typ = { "Typst", "https://styles.redditmedia.com/t5_83esp8/styles/communityIcon_eytolmfg1gra1.png" },
       },
       neovim_image_text = splashes[math.random(#splashes)]
     })
